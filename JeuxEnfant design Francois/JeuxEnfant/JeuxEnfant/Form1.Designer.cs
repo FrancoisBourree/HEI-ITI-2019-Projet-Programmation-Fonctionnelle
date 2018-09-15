78a5bde43f63a0752400be1ad5dd6aea6e32a0a2
Bourree <francois-bourree@orange.fr>
2018-09-15 15:22:32 +0200

    Application design

---
+++ JeuxEnfant design Francois/JeuxEnfant/JeuxEnfant/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace JeuxEnfant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.answerNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.levelButton2 = new System.Windows.Forms.Button();
+            this.levelButton3 = new System.Windows.Forms.Button();
+            this.levelButton4 = new System.Windows.Forms.Button();
+            this.levelButton5 = new System.Windows.Forms.Button();
+            this.levelButton6 = new System.Windows.Forms.Button();
+            this.levelButton7 = new System.Windows.Forms.Button();
+            this.levelButton8 = new System.Windows.Forms.Button();
+            this.levelButton9 = new System.Windows.Forms.Button();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.levelButtonSetter = new System.Windows.Forms.Button();
+            this.questionTextBox = new System.Windows.Forms.TextBox();
+            this.groupBoxLevel = new System.Windows.Forms.GroupBox();
+            this.groupBoxAnswer = new System.Windows.Forms.GroupBox();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.groupBoxQuestion = new System.Windows.Forms.GroupBox();
+            this.levelButton1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.answerNumericUpDown)).BeginInit();
+            this.groupBoxLevel.SuspendLayout();
+            this.groupBoxAnswer.SuspendLayout();
+            this.groupBoxQuestion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(310, 401);
+            this.progressBar.Maximum = 10;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(213, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // answerNumericUpDown
+            // 
+            this.answerNumericUpDown.Location = new System.Drawing.Point(355, 308);
+            this.answerNumericUpDown.Name = "answerNumericUpDown";
+            this.answerNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.answerNumericUpDown.TabIndex = 3;
+            this.answerNumericUpDown.ValueChanged += new System.EventHandler(this.answerNumericUpDown_ValueChanged);
+            // 
+            // levelButton2
+            // 
+            this.levelButton2.Location = new System.Drawing.Point(117, 32);
+            this.levelButton2.Name = "levelButton2";
+            this.levelButton2.Size = new System.Drawing.Size(75, 23);
+            this.levelButton2.TabIndex = 1;
+            this.levelButton2.Text = "2";
+            this.levelButton2.UseVisualStyleBackColor = true;
+            this.levelButton2.Click += new System.EventHandler(this.levelButton2_Click);
+            // 
+            // levelButton3
+            // 
+            this.levelButton3.Location = new System.Drawing.Point(198, 32);
+            this.levelButton3.Name = "levelButton3";
+            this.levelButton3.Size = new System.Drawing.Size(75, 23);
+            this.levelButton3.TabIndex = 1;
+            this.levelButton3.Text = "3";
+            this.levelButton3.UseVisualStyleBackColor = true;
+            this.levelButton3.Click += new System.EventHandler(this.levelButton3_Click);
+            // 
+            // levelButton4
+            // 
+            this.levelButton4.Location = new System.Drawing.Point(279, 32);
+            this.levelButton4.Name = "levelButton4";
+            this.levelButton4.Size = new System.Drawing.Size(75, 23);
+            this.levelButton4.TabIndex = 1;
+            this.levelButton4.Text = "4";
+            this.levelButton4.UseVisualStyleBackColor = true;
+            this.levelButton4.Click += new System.EventHandler(this.levelButton4_Click);
+            // 
+            // levelButton5
+            // 
+            this.levelButton5.Location = new System.Drawing.Point(360, 32);
+            this.levelButton5.Name = "levelButton5";
+            this.levelButton5.Size = new System.Drawing.Size(75, 23);
+            this.levelButton5.TabIndex = 1;
+            this.levelButton5.Text = "5";
+            this.levelButton5.UseVisualStyleBackColor = true;
+            this.levelButton5.Click += new System.EventHandler(this.levelButton5_Click);
+            // 
+            // levelButton6
+            // 
+            this.levelButton6.Location = new System.Drawing.Point(441, 32);
+            this.levelButton6.Name = "levelButton6";
+            this.levelButton6.Size = new System.Drawing.Size(75, 23);
+            this.levelButton6.TabIndex = 1;
+            this.levelButton6.Text = "6";
+            this.levelButton6.UseVisualStyleBackColor = true;
+            this.levelButton6.Click += new System.EventHandler(this.levelButton6_Click);
+            // 
+            // levelButton7
+            // 
+            this.levelButton7.Location = new System.Drawing.Point(522, 32);
+            this.levelButton7.Name = "levelButton7";
+            this.levelButton7.Size = new System.Drawing.Size(75, 23);
+            this.levelButton7.TabIndex = 1;
+            this.levelButton7.Text = "7";
+            this.levelButton7.UseVisualStyleBackColor = true;
+            this.levelButton7.Click += new System.EventHandler(this.levelButton7_Click);
+            // 
+            // levelButton8
+            // 
+            this.levelButton8.Location = new System.Drawing.Point(603, 32);
+            this.levelButton8.Name = "levelButton8";
+            this.levelButton8.Size = new System.Drawing.Size(75, 23);
+            this.levelButton8.TabIndex = 1;
+            this.levelButton8.Text = "8";
+            this.levelButton8.UseVisualStyleBackColor = true;
+            this.levelButton8.Click += new System.EventHandler(this.levelButton8_Click);
+            // 
+            // levelButton9
+            // 
+            this.levelButton9.Location = new System.Drawing.Point(684, 32);
+            this.levelButton9.Name = "levelButton9";
+            this.levelButton9.Size = new System.Drawing.Size(75, 23);
+            this.levelButton9.TabIndex = 1;
+            this.levelButton9.Text = "9";
+            this.levelButton9.UseVisualStyleBackColor = true;
+            this.levelButton9.Click += new System.EventHandler(this.levelButton9_Click);
+            // 
+            // answerButton
+            // 
+            this.answerButton.Location = new System.Drawing.Point(353, 72);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(99, 39);
+            this.answerButton.TabIndex = 4;
+            this.answerButton.Text = "Répondre";
+            this.answerButton.UseVisualStyleBackColor = true;
+            this.answerButton.Click += new System.EventHandler(this.answerButton_Click);
+            // 
+            // levelButtonSetter
+            // 
+            this.levelButtonSetter.Location = new System.Drawing.Point(334, 76);
+            this.levelButtonSetter.Name = "levelButtonSetter";
+            this.levelButtonSetter.Size = new System.Drawing.Size(129, 57);
+            this.levelButtonSetter.TabIndex = 2;
+            this.levelButtonSetter.Text = "C\'est parti !";
+            this.levelButtonSetter.UseVisualStyleBackColor = true;
+            this.levelButtonSetter.Click += new System.EventHandler(this.levelButtonSetter_Click);
+            // 
+            // questionTextBox
+            // 
+            this.questionTextBox.Location = new System.Drawing.Point(321, 45);
+            this.questionTextBox.Name = "questionTextBox";
+            this.questionTextBox.Size = new System.Drawing.Size(156, 20);
+            this.questionTextBox.TabIndex = 99;
+            this.questionTextBox.TabStop = false;
+            this.questionTextBox.TextChanged += new System.EventHandler(this.questionTextBox_TextChanged);
+            // 
+            // groupBoxLevel
+            // 
+            this.groupBoxLevel.Controls.Add(this.levelButton2);
+            this.groupBoxLevel.Controls.Add(this.levelButton3);
+            this.groupBoxLevel.Controls.Add(this.levelButton4);
+            this.groupBoxLevel.Controls.Add(this.levelButton5);
+            this.groupBoxLevel.Controls.Add(this.levelButton6);
+            this.groupBoxLevel.Controls.Add(this.levelButton7);
+            this.groupBoxLevel.Controls.Add(this.levelButton8);
+            this.groupBoxLevel.Controls.Add(this.levelButton9);
+            this.groupBoxLevel.Controls.Add(this.levelButton1);
+            this.groupBoxLevel.Controls.Add(this.levelButtonSetter);
+            this.groupBoxLevel.Location = new System.Drawing.Point(12, 16);
+            this.groupBoxLevel.Name = "groupBoxLevel";
+            this.groupBoxLevel.Size = new System.Drawing.Size(776, 150);
+            this.groupBoxLevel.TabIndex = 14;
+            this.groupBoxLevel.TabStop = false;
+            this.groupBoxLevel.Text = "Niveau";
+            // 
+            // groupBoxAnswer
+            // 
+            this.groupBoxAnswer.Controls.Add(this.answerButton);
+            this.groupBoxAnswer.Location = new System.Drawing.Point(12, 262);
+            this.groupBoxAnswer.Name = "groupBoxAnswer";
+            this.groupBoxAnswer.Size = new System.Drawing.Size(776, 133);
+            this.groupBoxAnswer.TabIndex = 15;
+            this.groupBoxAnswer.TabStop = false;
+            this.groupBoxAnswer.Text = "Réponse";
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(367, 16);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(55, 13);
+            this.labelQuestion.TabIndex = 16;
+            this.labelQuestion.Text = "Question :";
+            // 
+            // groupBoxQuestion
+            // 
+            this.groupBoxQuestion.Controls.Add(this.questionTextBox);
+            this.groupBoxQuestion.Controls.Add(this.labelQuestion);
+            this.groupBoxQuestion.Location = new System.Drawing.Point(12, 172);
+            this.groupBoxQuestion.Name = "groupBoxQuestion";
+            this.groupBoxQuestion.Size = new System.Drawing.Size(776, 84);
+            this.groupBoxQuestion.TabIndex = 17;
+            this.groupBoxQuestion.TabStop = false;
+            this.groupBoxQuestion.Text = "Question";
+            // 
+            // levelButton1
+            // 
+            this.levelButton1.Location = new System.Drawing.Point(36, 32);
+            this.levelButton1.Name = "levelButton1";
+            this.levelButton1.Size = new System.Drawing.Size(75, 23);
+            this.levelButton1.TabIndex = 3;
+            this.levelButton1.Text = "1";
+            this.levelButton1.UseVisualStyleBackColor = true;
+            this.levelButton1.Click += new System.EventHandler(this.levelButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.answerNumericUpDown);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.groupBoxLevel);
+            this.Controls.Add(this.groupBoxAnswer);
+            this.Controls.Add(this.groupBoxQuestion);
+            this.Name = "Form1";
+            this.Text = "Le jeux";
+            ((System.ComponentModel.ISupportInitialize)(this.answerNumericUpDown)).EndInit();
+            this.groupBoxLevel.ResumeLayout(false);
+            this.groupBoxAnswer.ResumeLayout(false);
+            this.groupBoxQuestion.ResumeLayout(false);
+            this.groupBoxQuestion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.NumericUpDown answerNumericUpDown;
+        private System.Windows.Forms.Button levelButton2;
+        private System.Windows.Forms.Button levelButton3;
+        private System.Windows.Forms.Button levelButton4;
+        private System.Windows.Forms.Button levelButton5;
+        private System.Windows.Forms.Button levelButton6;
+        private System.Windows.Forms.Button levelButton7;
+        private System.Windows.Forms.Button levelButton8;
+        private System.Windows.Forms.Button levelButton9;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.Button levelButtonSetter;
+        private System.Windows.Forms.TextBox questionTextBox;
+        private System.Windows.Forms.GroupBox groupBoxLevel;
+        private System.Windows.Forms.GroupBox groupBoxAnswer;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.GroupBox groupBoxQuestion;
+        private System.Windows.Forms.Button levelButton1;
+    }
+}
+
---
+++ JeuxEnfant design Francois/JeuxEnfant/JeuxEnfant/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuxEnfant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Les levelButton permettent de fixer le Int du niveau
+
+        private void levelButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void levelButton9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // levelButtonSetter lance la génération des nombres, de l'opérateur et du résultat
+
+        private void levelButtonSetter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // questionTextBox affiche la question posée à l'enfant
+
+        private void questionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // answerNumericUpDown permet à l'enfant de saisir un Int comme réponse
+
+        private void answerNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // answerButton permet d'envoyer ce qui a été rentré dans answerNumericUpDown et de le comparer au résultat attendu
+        // il lance la génération des nombres, de l'opérateur et du résultat
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // progressBar montre l'ancement des quesions : s'incrémente de 1 pour une bonne réponse, retombe à 0 si 2 mauvaises réponses successives et a un max de 10
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
